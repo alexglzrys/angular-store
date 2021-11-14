17cb196721897b9f18043f042a64814fb59f96c7
Alejandro González Reyes <alexglzrys@gmail.com>
2021-11-14 16:26:19 -0600

    pipes en programación reactiva

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { CartService } from 'src/app/core/services/cart/cart.service';
 
 @Component({
@@ -11,12 +13,18 @@ export class HeaderComponent implements OnInit {
   total_products: number;
   total: number
 
+  total$: Observable<number>;
+
   constructor(private cartServices: CartService) {
     // Nos suscribimos al carrito para saber cuando cambia su contenido. En este caso cuando se invoca next(valor) se ejecuta esta parte de código, de esta forma sabemos cuando elementos hay en la lista de productos
     this.cartServices.cart$.subscribe(products => {
       console.log(products);
       this.total_products = products.length;
     })
+
+    // Segunda forma: Susbribirse a un observable directamente desde al vista
+    // Uso un pipe ya que deseo transformar el flujo de datos, en este caso deseo mapearlo y solo retornar el nùmero de elementos del arreglo entregado como respuesta
+    this.total$ = this.cartServices.cart$.pipe(map(products => products.length));
   }
 
   ngOnInit(): void {
