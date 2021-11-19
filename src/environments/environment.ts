fd3ba5f3e7cab3d287d01938e285b7f84e6de8bf
Alejandro González Reyes <alexglzrys@gmail.com>
2021-11-19 11:05:54 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,17 +4,17 @@
 
 export const environment = {
   production: false,
-  api_url: 'https://platzi-store.herokuapp.com',
+  api_url: '',
   // Confifuración del proyecto en Firebase
   firebase: {
-    apiKey: 'AIzaSyAOEHxUBemyD8Av9rJWMvQs29NtBSkxqK8',
-    authDomain: 'angular-training-store.firebaseapp.com',
-    //databaseURL: '<your-database-URL>',
-    projectId: 'angular-training-store',
-    storageBucket: 'angular-training-store.appspot.com',
-    messagingSenderId: '509895602841',
-    appId: '1:509895602841:web:6720fd2e696d61bb543e08',
-    measurementId: 'G-XV0F2M865Z'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   }
 };
 
